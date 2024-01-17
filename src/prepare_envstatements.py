ebc177a28a4d178ad7cb40469f50049f6f2f7c38
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2024-01-17 17:45:34 +0000

    Started parsing sections for the environment statements institution level.

--- src/prepare_envstatements.py
+++ src/prepare_envstatements.py
@@ -3,6 +3,7 @@ import argparse
 from io import StringIO
 import sys
 import os
+from operator import add
 import pandas as pd
 
 import read_write as rw
@@ -10,34 +11,81 @@ import codebook as cb
 import preprocess as pp
 import logs as lg
 
-TO_DELETE_HEADERS = [
-    "Institutional level environment template (REF5a)",
-    "Unit-level environment template (REF5b)"
-]
+# include all that was noticed in the actual statements
+TO_DELETE_HEADERS = [item.lower()
+                     for item in [
+                                "Institutional level environment template (REF5a)",
+                                "Institutional level environment template (REF5b)",
+                                "Unit-level environment template (REF5a)",
+                                "Unit-level environment template (REF5b)",
+                                "REF5a - Institution Environment Statement",
+                                'Institutional-Level Environment Statement (REF5a)']
+                    ]
 
-TO_DELETE_PAGES = [
-    "Page 1", "Page 2", "Page 3", "Page 4", "Page 5",
-    "Page 6", "Page 7", "Page 8", "Page 9", "Page 10",
-    "Page 11", "Page 12", "Page 13", "Page 14", "Page 15",
-    "Page 16", "Page 17", "Page 18", "Page 19", "Page 20",
-    "Page 21", "Page 22", "Page 23", "Page 24", "Page 25",
-    "Page 26", "Page 27", "Page 28", "Page 29", "Page 30",
-    "Page 31", "Page 32", "Page 33", "Page 34", "Page 35",
-    "Page 36", "Page 37", "Page 38", "Page 39", "Page 40",
-]
+TO_DELETE_PAGES = [f"Page {i}" for i in range(1, 100)]
+
 
 TO_DELETE_CHARS = ['\n', '\t']
 
+SECTIONS = {
+    'Context and mission': [
+        '1. Institutional context and mission',
+        '1. Context and mission',
+        '1 Context and mission',
+        '1: Context and mission',
+        'a) Context and mission',
+        'Context and mission',
+        'Section 1: Context and mission',
+        '1. Context and Vision',
+        '1. Institutional overview',
+        '1, 2. Context and mission; Strategy',
+        '1B 1. Context and mission'],
+    'Strategy': [
+        '2. Strategy',
+        '2 Strategy',
+        '2: Strategy',
+        'b) Strategy',
+        '2. Research and Impact Strategy',
+        '2. Research strategy',
+        '2B 2. Strategy',
+        'Section 2: Strategy',
+        'Strategy',
+        '2. Strategy and Achievements',
+        '2. Institutional Research and Impact Strategy',
+        '2. Research and Knowledge Exchange Strategy'],
+    'People': [
+        '3. People',
+        '3 People',
+        '3: People',
+        'c) People',
+        '3B 3. People',
+        'Section 3: People',
+        'People',
+        '3. Staffing'],
+    'Income, infrastructure and facilities': [
+        '4. Income, infrastructure and facilities',
+        '4 Income, infrastructure and facilities',
+        '4: Income, infrastructure and facilities',
+        'd) Income, infrastructure and facilities',
+        '4B 4. Income, infrastructure and facilities',
+        'Section 4: Income, infrastructure and facilities',
+        'Income, infrastructure and facilities',
+        '4. Income, infrastructure, and facilities',
+        'd) Income, infrastructure, and facilities',
+        '4.1 Income'
+        ]
+}
+
 
 def clean_content(content):
-    """ Cleans the content of the environment statement. 
-    
+    """ Cleans the content of the environment statement.
+
     Args:
 
         content (str): the content of the environment statement
 
     Returns:
-    
+
             str: the cleaned content
     """
 
@@ -59,26 +107,63 @@ def clean_content(content):
     return content
 
 
+def institution_sections(statement, fname):
+
+    counts = [0 for section in SECTIONS]
+    content = ['' for section in SECTIONS]
+
+    # split the statement into lines
+    lines = statement.splitlines()
+
+    # delete empty lines
+    lines = [line for line in lines if line.strip()]
+
+    # replace tabs with spaces
+    lines = [line.replace('\t', ' ') for line in lines]
+
+    # replace multiple spaces with a single space
+    lines = [' '.join(line.split()) for line in lines]
+
+    # delete lines with specified content
+    lines = [line for line in lines if line.lower() not in TO_DELETE_PAGES]
+    lines = [line for line in lines if line.lower() not in TO_DELETE_HEADERS]
+
+    for isection, (section, headers) in enumerate(SECTIONS.items()):
+        for header in headers:
+            if header.lower() in [line.lower() for line in lines]:
+                counts[isection] += 1
+                break
+
+    return (counts, content)
+
+
 def prepare_institution_statements(prefix="Institution environment statement - ", extension=".txt"):
 
     lg.print_tstamp("PPROC actions for organization envinroment statements")
     inputpath = os.path.join(rw.PROCESSED_ENV_EXTRACTED_PATH, "institution")
     outputpath = rw.PROCESSED_ENV_PREPARED_PATH
+    # get the file names
     fnames = os.listdir(os.path.join(rw.PROJECT_PATH, inputpath))
-    lg.print_tstamp(f"- '{inputpath}' statements: {len(fnames)}")
+    statement_count = len(fnames)
     fnames = [fname.replace(prefix, "").replace(extension, "") for fname in fnames]
 
+    lg.print_tstamp(f"- '{inputpath}' statements: {statement_count}")
+
     dset = pd.DataFrame()
+    counts = [0 for section in SECTIONS]
     for fname in fnames:
         institution_name = fname
         infname = os.path.join(inputpath, f"{prefix}{fname}{extension}")
-        
+
         with open(infname, 'r+') as file:
-            content = file.read()
-            content = clean_content(content)
+            statement = file.read()
+
+            (fcounts, fcontent) = institution_sections(statement, fname)
+            counts = list(map(add, counts, fcounts))
+
         dset = pd.concat([dset,
                           pd.DataFrame({cb.COL_INST_NAME: [institution_name],
-                                        cb.COL_ENV_STATEMENT: [content]}
+                                        cb.COL_ENV_STATEMENT: [statement]}
                                        )],
                          ignore_index=True)
     lg.print_tstamp(f"- aggregated {dset.shape[0]} statements")
@@ -122,8 +207,7 @@ def prepare_unit_statements(prefix="Unit environment statement - ", extension=".
     fname = os.path.join(rw.PROJECT_PATH, outputpath,
                          f"{fname_root}.csv.gz")
     lg.print_tstamp(f"- write to '{fname}'")
-    dset.to_csv(fname, index=False,
-                                     compression='gzip')
+    dset.to_csv(fname, index=False, compression='gzip')
 
     # split the DataFrame in four and save them to separate CSV files
     quarter_index = len(dset) // 4
