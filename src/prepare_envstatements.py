3e30852a8e54a31dd801bf115cd1a674fba8960c
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2024-01-19 11:43:31 +0000

    Updated the prepared statements.

--- src/prepare_envstatements.py
+++ src/prepare_envstatements.py
@@ -19,14 +19,14 @@ TO_DELETE_HEADERS = [item.replace(" ", "").lower()
                                 "REF5a - Institution Environment Statement",
                                 'Institutional-Level Environment Statement (REF5a)',]]
 
+CHARS_TO_DELETE_FROM_HEADER = [" ", "\t", ".", ",", ":", ";", "-", "and", "&"]
+
 TO_DELETE_PAGES = [f"Page{i}".lower() for i in range(1, 100)]
 
 SECTIONS_INSTITUTION = {
     'Context and mission': [
         '1. Institutional context and mission',
         '1. Context and mission',
-        '1 Context and mission',
-        '1: Context and mission',
         'a) Context and mission',
         'Context and mission',
         'Section 1: Context and mission',
@@ -36,8 +36,6 @@ SECTIONS_INSTITUTION = {
         '1B 1. Context and mission'],
     'Strategy': [
         '2. Strategy',
-        '2 Strategy',
-        '2: Strategy',
         'b) Strategy',
         '2. Research and Impact Strategy',
         '2. Research strategy',
@@ -47,11 +45,10 @@ SECTIONS_INSTITUTION = {
         '2. Strategy and Achievements',
         '2. Institutional Research and Impact Strategy',
         '2. Research and Knowledge Exchange Strategy',
-        'Research at Cambridge'],
+        'Research at Cambridge'  # Cambridge merges the first two sections into one section
+        ],
     'People': [
         '3. People',
-        '3 People',
-        '3: People',
         'c) People',
         '3B 3. People',
         'Section 3: People',
@@ -59,14 +56,10 @@ SECTIONS_INSTITUTION = {
         '3. Staffing'],
     'Income, infrastructure and facilities': [
         '4. Income, infrastructure and facilities',
-        '4 Income, infrastructure and facilities',
-        '4: Income, infrastructure and facilities',
         'd) Income, infrastructure and facilities',
         '4B 4. Income, infrastructure and facilities',
         'Section 4: Income, infrastructure and facilities',
         'Income, infrastructure and facilities',
-        '4. Income, infrastructure, and facilities',
-        'd) Income, infrastructure, and facilities',
         '4.1 Income'
         ]
 }
@@ -93,6 +86,7 @@ SECTIONS_UNIT = {
         '1 Unit context and structure, research and impact',
         '1B 1. Unit context and structure, research and impact strategy',
         '2B 1. Unit context and structure, research and impact strategy',
+        'Section 1.',
         'Section 1. Unit context and structure, research and impact strategy',
         'Section 1. Unit structure, research and impact strategy',
         'Section 1: Unit Context, Research and Impact Strategy',
@@ -100,12 +94,11 @@ SECTIONS_UNIT = {
         'Section 1: Context and Structure',
         'Section 1: Unit Context and Structure',
         'Section 1 (S1). Unit context and structure, research and impact strategy',
-        '1.1. Overview',
-        'Overview',
         'Unit Context',
         'Unit context and structure',
         'Unit context and structure, research and impact strategy',
-        '1. Unit context, structure, research, open research environment, strategic aims and',  # two lines
+        '1. Unit context, structure, research, open research environment, '\
+        'strategic aims and',  # two lines
         '• Unit context and structure, research and impact strategy'
     ],
     'People': [
@@ -124,7 +117,8 @@ SECTIONS_UNIT = {
         "2.0 People",
         '2B 2. People',
         '3B 2. People',
-        'Section 2: People – Note: All staff-related data in this section refer to Cat. A submitted staff',
+        'Section 2: People – Note: All staff-related data in this section refer to '\
+        'Cat. A submitted staff',
     ],
     'Income, infrastructure and facilities': [
         '3. Income, infrastructure and facilities',
@@ -150,7 +144,7 @@ SECTIONS_UNIT = {
         '4. Section 4: Collaboration and contribution to the research base, economy and society',
         '4. Collaboration with and contribution to the research base, economy and society',
         '4. Collaborations and contributions to the research base, economy and society',
-        '4. Collaboration, impact, engagement, responsiveness to emerging priorities and',  # two lines
+        '4. Collaboration, impact, engagement, responsiveness to emerging priorities and',
         '4. Collaboration and contribution to the research base, the economy and to society',
         '4.0 Collaboration and contribution to the research base, economy and society',
         '2B 4. Collaboration and contribution to the research base, economy and society',
@@ -192,10 +186,8 @@ def get_and_clean_lines(statement):
 
 def clean_header(header):
 
-    CHARS_TO_DELETE = [" ", "\t", ".", ",", ":", ";", "-", "and", "&"]
-
     header = header.lower()
-    for chars in CHARS_TO_DELETE:
+    for chars in CHARS_TO_DELETE_FROM_HEADER:
         header = header.replace(chars, "")
 
     return header
@@ -209,7 +201,7 @@ def section_indices(statement, sections):
 
     for isection, (section, headers) in enumerate(sections.items()):
         headers_to_compare = [clean_header(header) for header in headers]
-        
+
         for header in headers_to_compare:
             for iline, line in enumerate(lines):
                 if header == clean_header(line):
@@ -231,7 +223,9 @@ def prepare_institution_statements(prefix="Institution environment statement - "
     statement_count = len(fnames)
     fnames = [fname.replace(prefix, "").replace(extension, "") for fname in fnames]
 
-    lg.print_tstamp(f"- '{inputpath}' statements: {statement_count}")
+    lg.print_tstamp(f"- read data from '{inputpath}' ")
+    lg.print_tstamp(f"- statements: {statement_count}, "
+                    f"sections: {len(SECTIONS_INSTITUTION.keys())}")
 
     dset = pd.DataFrame()
     counts = [0 for section in SECTIONS_INSTITUTION]
@@ -260,7 +254,8 @@ def prepare_institution_statements(prefix="Institution environment statement - "
 
         dset = pd.concat([dset, pd.DataFrame(data)], ignore_index=True)
 
-    lg.print_tstamp(f"- prepared institution statements: {dset.shape[0]}")
+    lg.print_tstamp(f"- prepared institution statements: {dset.shape[0]} records, "
+                    f"{dset.shape[1]} columns")
     if dset.shape[0] != statement_count:
         lg.print_tstamp(f"- WARNING: prepared institution statements "
                         f"{dset.shape[0]}/{statement_count}")
@@ -282,7 +277,8 @@ def prepare_unit_statements(prefix="Unit environment statement - ", extension=".
     statement_count = len(fnames)
     fnames = [fname.replace(prefix, "").replace(extension, "") for fname in fnames]
 
-    lg.print_tstamp(f"- '{inputpath}' statements: {statement_count}")
+    lg.print_tstamp(f"- read data from '{inputpath}' ")
+    lg.print_tstamp(f"- statements: {statement_count}, sections: {len(SECTIONS_UNIT.keys())}")
 
     dset = pd.DataFrame()
     counts = [0 for section in SECTIONS_UNIT]
@@ -296,7 +292,6 @@ def prepare_unit_statements(prefix="Unit environment statement - ", extension=".
         with open(infname, 'r+') as file:
             statement = file.read()
             (indices, lines) = section_indices(statement, SECTIONS_UNIT)
-            
 
         data = {cb.COL_INST_NAME: [institution_name],
                 cb.COL_UOA_NAME: [unit_name]
@@ -313,85 +308,21 @@ def prepare_unit_statements(prefix="Unit environment statement - ", extension=".
                 section_content = None
             data[section] = section_content
             if section_content is None:
-                lg.print_tstamp(f"- WARNING: no content for '{section}' in '{institution_name}' - '{unit_code}'")
-                print(infname)
-                
+                lg.print_tstamp(f"- WARNING: no content for "
+                                f"'{section}' in '{institution_name}' - '{unit_code}'")
 
         dset = pd.concat([dset, pd.DataFrame(data)], ignore_index=True)
-    print(counts, statement_count)
-    # lg.print_tstamp(f"- prepared institution statements: {dset.shape[0]}")
-    # if dset.shape[0] != statement_count:
-    #     lg.print_tstamp(f"- WARNING: prepared institution statements "
-    #                     f"{dset.shape[0]}/{statement_count}")
 
-    # fname = os.path.join(rw.PROJECT_PATH, outputpath,
-    #                      "EnvStatementsInstitutionLevel.csv.gz")
-    # lg.print_tstamp(f"- write to '{fname}'")
-    # dset.to_csv(fname, index=False,
-    #             compression='gzip')
+    lg.print_tstamp(f"- prepared institution statements: {dset.shape[0]}")
+    if dset.shape[0] != statement_count:
+        lg.print_tstamp(f"- WARNING: prepared institution statements "
+                        f"{dset.shape[0]}/{statement_count}")
 
-
-# def prepare_unit_statements_old(prefix="Unit environment statement - ", extension=".txt"):
-
-#     lg.print_tstamp("PPROC actions for unit envinroment statements")
-#     inputpath = os.path.join(rw.PROCESSED_ENV_EXTRACTED_PATH, "unit")
-#     outputpath = rw.PROCESSED_ENV_PREPARED_PATH
-#     fnames = os.listdir(os.path.join(rw.PROJECT_PATH, inputpath))
-#     lg.print_tstamp(f"- '{inputpath}' statements: {len(fnames)}")
-#     fnames = [fname.replace(prefix, "").replace(extension, "") for fname in fnames]
-
-#     dset = pd.DataFrame()
-#     for fname in fnames:
-#         institution_name, unit_code = fname.split(" - ")
-#         # strip any alphabetical characters from the unit code
-#         unit_code = ''.join([i for i in unit_code if not i.isalpha()])
-#         unit_name = cb.UOA_NAMES[int(unit_code)]
-
-#         infname = os.path.join(inputpath, f"{prefix}{fname}{extension}")
-
-#         with open(infname, 'r+') as file:
-#             content = file.read()
-#             content = clean_content(content)
-#         dset = pd.concat([dset,
-#                           pd.DataFrame({cb.COL_INST_NAME: [institution_name],
-#                                         cb.COL_UOA_NAME: [unit_name],
-#                                         cb.COL_ENV_STATEMENT: [content]}
-#                                        )],
-#                          ignore_index=True)
-#     lg.print_tstamp(f"- aggregated {dset.shape[0]} statements")
-
-#     fname_root = "EnvStatementsUnitLevel"
-#     # save all the statements to a single csv file
-#     fname = os.path.join(rw.PROJECT_PATH, outputpath,
-#                          f"{fname_root}.csv.gz")
-#     lg.print_tstamp(f"- write to '{fname}'")
-#     dset.to_csv(fname, index=False, compression='gzip')
-
-#     # split the DataFrame in four and save them to separate CSV files
-#     quarter_index = len(dset) // 4
-#     fname = os.path.join(rw.PROJECT_PATH, outputpath,
-#                          f"{fname_root}_1.csv.gz")
-#     lg.print_tstamp(f"- write to '{fname}'")
-#     dset.iloc[:quarter_index].to_csv(fname, index=False,
-#                                      compression='gzip')
-
-#     fname = os.path.join(rw.PROJECT_PATH, outputpath,
-#                          f"{fname_root}_2.csv.gz")
-#     lg.print_tstamp(f"- write to '{fname}'")
-#     dset.iloc[quarter_index:2*quarter_index].to_csv(fname, index=False,
-#                                                     compression='gzip')
-
-#     fname = os.path.join(rw.PROJECT_PATH, outputpath,
-#                          f"{fname_root}_3.csv.gz")
-#     lg.print_tstamp(f"- write to '{fname}'")
-#     dset.iloc[2*quarter_index:3*quarter_index].to_csv(fname, index=False,
-#                                                       compression='gzip')
-
-#     fname = os.path.join(rw.PROJECT_PATH, outputpath,
-#                          f"{fname_root}_4.csv.gz")
-#     lg.print_tstamp(f"- write to '{fname}'")
-#     dset.iloc[3*quarter_index:].to_csv(fname, index=False,
-#                                        compression='gzip')
+    fname = os.path.join(rw.PROJECT_PATH, outputpath,
+                         "EnvStatementsUnitLevel.csv.gz")
+    lg.print_tstamp(f"- write to '{fname}'")
+    dset.to_csv(fname, index=False,
+                compression='gzip')
 
 
 if __name__ == "__main__":
